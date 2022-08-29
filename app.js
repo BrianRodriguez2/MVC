585220580c625478e6764b36553c793fc30f3491
Brian <rodriguezrbrian@gmail.com>
2022-08-29 10:53:12 -0300

    Añade 1st commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+const mainRouter = require('./routers/main');
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use('/', mainRouter); 
+
+app.listen(PORT, () => console.log(`
+    Servidor activo en puerto: ${PORT}
+    http://localhost:${PORT}
+`))
